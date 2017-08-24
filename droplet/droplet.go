efd0fb8b77b898cccdc91a412862ecd4b99ed6f0
sam <sam@redmoo.info>
2017-08-24 14:10:21 +0300

    Modified droplet.go
    Changed first capital letter in var names.
    add support.ValidateRegions() function.

--- droplet/droplet.go
+++ droplet/droplet.go
@@ -4,14 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/altnometer/godoapi/lib/support"
 )
 
 // ParseArgs handles os.Args and calls relevant functions in the package.
 func ParseArgs(args []string) {
 	dropCmd := flag.NewFlagSet("droplet", flag.ExitOnError)
 	// droplet  subcommand flag pointers
-	NamePtr := dropCmd.String("name", "", "-name=<volname1[,volname2...]>")
-	RegPtr := dropCmd.String("region", "fra1", "-region=fra1")
+	namePtr := dropCmd.String("name", "", "-name=<volname1[,volname2...]>")
+	regPtr := dropCmd.String("region", "fra1", "-region=fra1")
 	dropCmd.Parse(args)
 	if len(args) < 1 {
 		fmt.Println("Provide the args, please.")
@@ -19,12 +21,13 @@ func ParseArgs(args []string) {
 		os.Exit(1)
 	}
 	if dropCmd.Parsed() {
-		if *NamePtr == "" {
+		if *namePtr == "" {
 			dropCmd.PrintDefaults()
 			os.Exit(1)
 		}
 	}
-	fmt.Printf("*NamePtr = %+v\n", *NamePtr)
-	fmt.Printf("*RegPtr = %+v\n", *RegPtr)
+	support.ValidateRegions(regPtr)
+	fmt.Printf("*namePtr = %+v\n", *namePtr)
+	fmt.Printf("*regPtr = %+v\n", *regPtr)
 
 }
