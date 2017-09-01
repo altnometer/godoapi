768bd49803b49272fa90a1fa113505d4345bbb37
sam <sam@redmoo.info>
2017-09-01 16:44:51 +0300

    Drafted integration of master and node setup for k8s

--- k8s/setupk8s/master.go
+++ k8s/setupk8s/master.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/altnometer/godoapi/droplet"
@@ -12,15 +13,7 @@ import (
 )
 
 // SetUpMaster would setup k8s master.
-func SetUpMaster(env, reg string) {
-	master1Name := "master-1"
-	runningMasters := *droplet.ReturnDropletsByTag("master")
-	for _, d := range runningMasters {
-		if d["Name"] == master1Name {
-			support.RedPf("Droplet with %s name already exist!", master1Name)
-			os.Exit(1)
-		}
-	}
+func SetUpMaster(env, reg string) (string, string) {
 	userName := os.Getenv("DOHostUsername")
 	if userName == "" {
 		support.YellowLn("You can set env var DOHostUsername!")
@@ -36,40 +29,58 @@ func SetUpMaster(env, reg string) {
 		support.YellowLn("You can set env var DOSSHKeyPath!")
 		sshKeyPath = support.GetUserInput("Type in a DOSSHKeyPath: ")
 	}
-
-	reqDataPtr := droplet.CreateRequestData
-	reqDataPtr.Size = "1gb"
-	reqDataPtr.Region = reg
-	reqDataPtr.Names = []string{"master-1"}
-	reqDataPtr.Tags = []string{"master", env}
-	drSpecs := droplet.CreateDroplet(reqDataPtr)
-	s := spinner.New(spinner.CharSets[9], 150*time.Millisecond)
-	s.Start()
-	// Give it some time for IPs to be assigned to the droplets.
-	time.Sleep(time.Second * 2)
-	s.Stop()
-	var IP string
-	for _, d := range drSpecs {
-		if d.Name != "" {
-			support.RedLn("Only SINGLE master is handled currently!!!")
-			dData := droplet.ReturnDropletByID(d.ID)
-			s.Start()
-			IP = dData["PublicIP"]
-			s.Stop()
-			break
+	sshCmdGetToken := []string{
+		"sudo",
+		"kubeadm", "token", "list", "|", "awk",
+		"'NR == 2 { printf $1 }'",
+	}
+	master1Name := "master-1"
+	runningMasters := *droplet.ReturnDropletsByTag("master")
+	var token string
+	var PublicIP string
+	for _, d := range runningMasters {
+		if d["Name"] == master1Name {
+			PublicIP = d["PublicIP"]
+			token = fetchSSHOutput("root", PublicIP, sshKeyPath, sshCmdGetToken)
+			support.YellowLn("Set env var for k8s token.")
+			os.Setenv("K8SToken", token)
+			support.RedPf("Droplet with %s name already exist!", master1Name)
+			// return d["PublicIP"], token
 		}
 	}
-	if IP == "" {
-		panic("No IP for k8s master host, cannot continue!")
-	} else {
-		fmt.Printf("IP = %+v\n", IP)
+	if token == "" {
+		reqDataPtr := droplet.CreateRequestData
+		reqDataPtr.Size = "1gb"
+		reqDataPtr.Region = reg
+		reqDataPtr.Names = []string{"master-1"}
+		reqDataPtr.Tags = []string{"master", env}
+		drSpecs := droplet.CreateDroplet(reqDataPtr)
+		s := spinner.New(spinner.CharSets[9], 150*time.Millisecond)
+		s.Start()
+		// Give it some time for IPs to be assigned to the droplets.
+		support.YellowLn("Initializing the droplet ...")
+		time.Sleep(time.Second * 10)
+		s.Stop()
+		for _, d := range drSpecs {
+			if d.Name != "" {
+				support.RedLn("Only SINGLE master is handled currently!!!")
+				dData := droplet.ReturnDropletByID(d.ID)
+				s.Start()
+				PublicIP = dData["PublicIP"]
+				s.Stop()
+				break
+			}
+		}
 	}
-	// execSSH(userName, IP, sshKeyPath)
+	if PublicIP == "" {
+		panic("No PublicIP for k8s master host, cannot continue!")
+	}
+	// execSSH(userName, PublicIP, sshKeyPath)
 	// bash master.sh --TARGET_MACHINE_IP 165.227.134.109 --PATH_TO_SSH_PRIV_KEYS ~/.ssh/circleci --USERNAME sally --USER_PASSWORD las
 	arg := []string{
 		"/home/sam/redmoo/devops/k8s/setupcluster/docean/master.sh",
 		"--TARGET_MACHINE_IP",
-		IP,
+		PublicIP,
 		"--PATH_TO_SSH_PRIV_KEYS",
 		sshKeyPath,
 		"--USERNAME",
@@ -77,18 +88,23 @@ func SetUpMaster(env, reg string) {
 		"--USER_PASSWORD",
 		userPassword,
 	}
+	argstr := strings.Join(arg, " ")
+	support.YellowLn("Executing ssh from golang with following args: ")
+	fmt.Printf("argstr = %+v\n", argstr)
 
 	// cmdOut, err := exec.Command("ssh", arg...).Output()
 	cmd := exec.Command("bash", arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	s.Start()
 	err := cmd.Run()
-	s.Stop()
 	if err != nil {
 		panic(err)
 	}
+	token = fetchSSHOutput("root", PublicIP, sshKeyPath, sshCmdGetToken)
+	support.YellowLn("Set env var for k8s token.")
+	os.Setenv("K8SToken", token)
+	return PublicIP, token
 }
 
 func execSSH(userName, IP, sshKeyPath string) {
@@ -141,19 +157,7 @@ func execSSH(userName, IP, sshKeyPath string) {
 	// 	fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
 	// 	os.Exit(1)
 	// }
-	// os.Exit(0)
-	// if err != nil {
-	// 	fmt.Printf("err = %+v\n", err)
-	// } else {
-	// 	fmt.Printf("cmdOut = %+v\n", string(cmdOut))
-	// }
-	// cmd := []string{
-	// 	"hostname",
-	// 	"-i",
-	// }
-	// err := sshCommander.Command(cmd...)
-	// cmdRes := sshCommander.Command(cmd...)
-	// fmt.Printf("cmdRes = %+v\n", cmdRes)
+}
 
 func fetchSSHOutput(userName, IP, sshKeyPath string, sshCmds []string) string {
 	cmdArgs := append(
