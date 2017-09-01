dd0d62c7712466f09f0fde5ae7d49a79fd772f45
sam <sam@redmoo.info>
2017-09-01 16:43:58 +0300

    Added node.go file to setup k8s node droplets.

---
+++ k8s/setupk8s/node.go
@@ -0,0 +1,35 @@
+package setupk8s
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/altnometer/godoapi/droplet"
+)
+
+var userDataPart1 = `#! /bin/bash
+
+apt-get update && apt-get upgrade -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF2 > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF2
+apt-get update -y
+apt-get install -y docker.io
+apt-get install -y --allow-unauthenticated kubelet kubeadm=1.7.0-00 kubectl kubernetes-cni`
+
+// SetUpNode would setup k8s master.
+func SetUpNode(env, reg, ip, token string) {
+	userDataPart2 := fmt.Sprintf("\nkubeadm join --token %s %s:6443", token, ip)
+	userData := fmt.Sprintln(userDataPart1, userDataPart2)
+	reqDataPtr := droplet.CreateRequestData
+	reqDataPtr.Size = "1gb"
+	reqDataPtr.Region = reg
+	reqDataPtr.Names = []string{"node-1"}
+	reqDataPtr.Tags = []string{"node", env}
+	reqDataPtr.UserData = userData
+	fmt.Printf("reqDataPtr = %+v\n", reqDataPtr)
+	drSpecs := droplet.CreateDroplet(reqDataPtr)
+	fmt.Printf("drSpecs = %+v\n", drSpecs)
+	os.Exit(0)
+}
